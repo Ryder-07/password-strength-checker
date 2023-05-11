b0cd14ca6217624920acbce7171290d288417306
Rudra Pratap Singh <65117190+Ryder-07@users.noreply.github.com>
2023-05-11 23:59:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 var password =document.getElementById("pass");
 var span = document.getElementById("strength");
 var para= document.getElementById("message"); 
-var show = document.querySelector(".show");
 var show_onn= document.querySelector(".show_on");
 var show_off= document.querySelector(".show_off");
 var opt1 = /[a-z]/;
 var opt2 = /\d+/;
 var opt3 =  /.[!,@,#,$,%,^,&,*,?,_,~,-,(,)]/;
 
-
+onload = function () {
+    
+    show_off.style.display="none";
+}
 function control(){
     if(password.value.length <=4 && (password.value.match(opt1)||password.value.match(opt2)||password.value.match(opt3))){
         password.style.borderBottomColor="#ff4757";
@@ -28,13 +30,12 @@ function control(){
         span.style.color="rgb(35,173,92)";
         para.style.display="block";
     }
-    show.style.display = "block";
 }
 function show_on(){
-    if (pass.type=="password"){
+    if (pass.type==="password"){
         pass.type="text";
-        show_off.style.display="block";
         show_onn.style.display="none";
+        show_off.style.display="block";
 
     }
     else{
